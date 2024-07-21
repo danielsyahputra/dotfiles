81c9d47bad0921d286ba44c6a1fd837204cc475b
daniel.syahputra <daniel.syahputra@ui.ac.id>
2024-07-21 17:38:32 +0700

    add some config (lsp, gitsigns, formatting, etc

--- .config/nvim/lua/danielsyahputra/lazy.lua
+++ .config/nvim/lua/danielsyahputra/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("danielsyahputra.plugins")
+require("lazy").setup({ { import = "danielsyahputra.plugins" }, { import = "danielsyahputra.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
