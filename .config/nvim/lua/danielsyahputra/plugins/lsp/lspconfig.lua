cbc1a203322e827597e15758c308c79f263eb548
danielsyahputra <dnlshp@gmail.com>
2025-08-30 00:10:43 +0700

    add javascript,typescript lsp server

--- .config/nvim/lua/danielsyahputra/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/danielsyahputra/plugins/lsp/lspconfig.lua
@@ -7,13 +7,10 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
 
-		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
 
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local keymap = vim.keymap -- for conciseness
@@ -21,11 +18,8 @@ return {
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 
-				-- set keybinds
 				opts.desc = "Show LSP references"
 				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
@@ -67,11 +61,8 @@ return {
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
@@ -79,14 +70,12 @@ return {
 		end
 
 		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
 			function(server_name)
 				lspconfig[server_name].setup({
 					capabilities = capabilities,
 				})
 			end,
 			["pyright"] = function()
-				-- configure emmet language server
 				lspconfig["pyright"].setup({
 					capabilities = capabilities,
 					filetypes = { "python" },
@@ -94,6 +83,21 @@ return {
 			end,
 			["clangd"] = function()
 				lspconfig["clangd"].setup({})
+			end,
+
+			["tsserver"] = function()
+				lspconfig["tsserver"].setup({
+					capabilities = capabilities,
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+					cmd = { "typescript-language-server", "--stdio" },
+				})
 			end,
 		})
 	end,
--- .config/nvim/lua/danielsyahputra/plugins/lsp/mason.lua
+++ .config/nvim/lua/danielsyahputra/plugins/lsp/mason.lua
@@ -5,15 +5,12 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
 
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
 
 		local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
 		mason.setup({
 			ui = {
 				icons = {
@@ -25,13 +22,13 @@ return {
 		})
 
 		mason_lspconfig.setup({
-			-- list of servers for mason to install
 			ensure_installed = {
 				"html",
 				"cssls",
 				"lua_ls",
 				"pyright",
 				"clangd",
+				"ts_ls",
 			},
 		})
 
